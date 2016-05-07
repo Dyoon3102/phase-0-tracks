7b0afcebbc45abaade21a7d8dd443fde9b69beb9
Daniel Yoon <sangchu3102@gmail.com>
2016-05-07 10:11:05 -0400

    Update with user interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -4,15 +4,6 @@
 		-Change each letter to the next letter in the alphabet
 =end
 
-=begin
-	-Ask the user to key in a word
-		-Loop through each letter of the word
-		-Change each letter to the previous letter in the alphabet
-=end
-
-#puts "Key in a password."
-#password = gets.chomp.downcase
-
 def encrypt(password)
 	index = 0
 	while index < password.length
@@ -30,6 +21,12 @@ puts ""
 encrypt("zed")
 puts ""
 
+=begin
+	-Ask the user to key in a word
+		-Loop through each letter of the word
+		-Change each letter to the previous letter in the alphabet
+=end
+
 def decrypt(password)
 	index = 0
 	while index < password.length
@@ -46,3 +43,20 @@ decrypt("bcd")
 puts ""
 decrypt("afe")
 puts ""
+
+# Ask the for choice of encrypt or decrypt
+puts "Would you like to encrypt(e) or decrypt(d) a password?"
+choice = gets.chomp
+
+# Ask the user to input a password
+puts "Key in your password."
+password = gets.chomp.downcase
+
+# Convert password to the method of choice
+if choice == "e"
+	puts encrypt(password)
+elsif choice == "d"
+	puts decrypt(password)
+end
+
+puts "Have a nice day!"
