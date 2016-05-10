e6c0497206e470bf52884fa8896ba10e748d1564
Daniel Yoon <sangchu3102@gmail.com>
2016-05-10 11:10:11 -0400

    Update file with methods

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,11 @@
+def array_sum(arr, a)
+	arr << a
+end
+
+def new_array(x, y, z)
+	[x, y, z]
+end
+
 our_array = []
 p our_array
 our_array << "item1" << "item2" << "item3" << "item4" << "item5"
@@ -15,3 +23,9 @@ end
 other_array = ["item7", "item8", "item9"]
 array_sum = our_array + other_array
 p array_sum
+
+p new_array("item1", "item2", "item3")
+
+p array_sum([], "item4")
+p array_sum(["item5", "item6", "item7"], "item4")
+p array_sum([["item4", "item5"], ["item6", "item7"]], ["item8", "item9", "item10"])
