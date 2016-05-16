4971edef2d55c1928cb1b9039811232f4ea7c5b5
Daniel Yoon <sangchu3102@gmail.com>
2016-05-16 06:48:33 -0400

    Create a user interface for the program

--- assessment/calculator.rb
+++ assessment/calculator.rb
@@ -10,7 +10,18 @@ def calculate(x, op, y)
 	end
 end
 
-puts calculate(5, "+", 4)
-puts calculate(32, "-", 13)
-puts calculate(3, "*", 6)
-puts calculate(33, "/", 11)
+#puts calculate(5, "+", 4)
+#puts calculate(32, "-", 13)
+#puts calculate(3, "*", 6)
+#puts calculate(33, "/", 11)
+
+puts "Enter a number:"
+num1 = gets.chomp.to_i
+
+puts "Enter an operator (+, -, *, /):"
+operator = gets.chomp.to_s
+
+puts "Enter another number:"
+num2 = gets.chomp.to_i
+
+puts calculate(num1, operator, num2)
