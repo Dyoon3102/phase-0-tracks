6c8ef2fdd66edf7b2d8747eca2377638f6feb942
Daniel Yoon <sangchu3102@gmail.com>
2016-05-16 13:47:03 -0400

    Create a class design for Puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES -----------------------
+# Hairless Shortleg
+
+# CHARACTERISTICS ---------------
+# Leg length: short
+# Friendly: yes
+# Tail length: short
+# Name: varies
+# Color: varies
+
+# BEHAVIOR ----------------------
+# Cuddle
+# Playful
+# Lick
