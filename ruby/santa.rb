a0b58dd7c8819e91735aa6b837938608b56175c6
Daniel Yoon <sangchu3102@gmail.com>
2016-05-18 09:36:55 -0400

    Add diverse initilization to the driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,11 @@
 class Santa
 	
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
 	end
-	
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -14,6 +16,19 @@ class Santa
 	
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("gingerbread cookie")
+#santa = Santa.new
+#santa.speak
+#santa.eat_milk_and_cookies("gingerbread cookie")
+
+santas = []
+reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+santas.each do |x|
+	x.speak
+	x.eat_milk_and_cookies("gingerbread cookie")
+end
