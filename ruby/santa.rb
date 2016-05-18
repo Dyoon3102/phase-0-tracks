e23e0f4cb2318643961748ef0f348a9c7a859844
Daniel Yoon <sangchu3102@gmail.com>
2016-05-18 12:41:43 -0400

    Update with release 2 and 3 requisites

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,31 +1,44 @@
 class Santa
 	
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
+	
+	# An initialize method that prints "Initializing Santa instance ...".
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
 	end
-
+	
+	# A speak method that will print "Ho, ho, ho! Haaaappy holidays!" 
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 	
+	# An eat_milk_and_cookies method that takes a cookie type (example: "snickerdoodle") as a parameter and prints "That was a good <type of cookie here>!"
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
 	
+	# Celebrate_birthday should age Santa by one year.
+	def celebrate_birthday(age)
+		age + 1
+	end
+	
+	# Take a reindeer's name as an argument, and move that reindeer in last place in the reindeer rankings.
+	def get_mad_at(reindeer)
+		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		reindeer_ranking.delete_if { |x| x == reindeer }
+		reindeer_ranking << reindeer
+	end
+	
 end
 
-#santa = Santa.new
-#santa.speak
-#santa.eat_milk_and_cookies("gingerbread cookie")
-
 santas = []
-reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicity = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+gender.length.times do |i|
+  santas << Santa.new(gender[i], ethnicity[i])
 end
 
 santas.each do |x|
