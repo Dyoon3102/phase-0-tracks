b64b8d95d92092a223ed387c4fc7c0ef864ca7c4
Daniel Yoon <sangchu3102@gmail.com>
2016-05-20 14:21:12 -0400

    Update file with yell_happily method

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,4 +2,11 @@ module Shout
 	def self.yell_angrily(words)
     	words + "!!!" + " :("
   	end
+
+  	def self.yell_happily(words)
+  		words + "!!!" + " :)"
+	end
 end
+
+puts Shout.yell_angrily("snap")
+puts Shout.yell_happily("whoo")
