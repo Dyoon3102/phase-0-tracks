90a66cb4ac27aa91068d65f99874ae1e10a117f2
Daniel Yoon <sangchu3102@gmail.com>
2016-05-20 14:39:25 -0400

    Add Driver and Kid class for Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,41 @@
+# Release 1
+# module Shout
+#	def self.yell_angrily(words)
+#    	words + "!!!" + " :("
+#  	end
+
+#  	def self.yell_happily(words)
+#  		words + "!!!" + " :)"
+#	end
+# end
+
+# puts Shout.yell_angrily("snap")
+# puts Shout.yell_happily("whoo")
+
+# Release 3
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
     	words + "!!!" + " :("
   	end
 
-  	def self.yell_happily(words)
+  	def yell_happily(words)
   		words + "!!!" + " :)"
 	end
 end
 
-puts Shout.yell_angrily("snap")
-puts Shout.yell_happily("whoo")
+class Driver
+	include Shout
+end
+
+class Kid
+	include Shout
+end
+
+driver = Driver.new
+puts driver.yell_angrily("Out of my way")
+puts driver.yell_happily("Wheee")
+
+kid = Kid.new
+puts kid.yell_angrily("I don't want broccoli")
+puts kid.yell_happily("It's candy")
