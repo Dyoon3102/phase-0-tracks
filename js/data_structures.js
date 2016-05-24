4c9cd87f043b66e58fbc29e4fcab5ad3078caed1
Daniel Yoon <sangchu3102@gmail.com>
2016-05-24 11:28:30 -0400

    Build an object using two arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,15 @@
 var colors = ["blue", "red", "cyan", "green"];
 var horseNames = ["Thunder", "Wildfire", "Whispers", "Inego Montoya"];
-colors.push("magenta");
-horseNames.push("Lefty");
-console.log(colors)
-console.log(horseNames)
+
+
+// colors.push("magenta");
+// horseNames.push("Lefty");
+// console.log(colors)
+// console.log(horseNames)
+// Notice the notation on line   : objName[[propertyKeyname]] = propertyValue[i]
+
+var horseColor = {}
+for (var i = 0; i < horseNames.length; i++) {
+  horseColor[horseNames[i]] = colors[i];
+}
+console.log(horseColor)
