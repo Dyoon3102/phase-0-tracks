a21e12c95daaf11adf036f6128c614069a6db61b
Daniel Yoon <sangchu3102@gmail.com>
2016-05-24 10:35:57 -0400

    Initial commit

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+var colors = ["blue", "red", "cyan", "green"];
+var horseNames = ["Thunder", "Wildfire", "Whispers", "Inego Montoya"];
+colors.push("magenta");
+horseNames.push("Lefty");
+console.log(colors)
+console.log(horseNames)
