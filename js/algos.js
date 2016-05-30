16f1430c22690f04a8e866295d275fd18adf1e04
Daniel Yoon <sangchu3102@gmail.com>
2016-05-30 12:38:43 -0400

    File correction

--- js/algos.js
+++ js/algos.js
@@ -16,12 +16,8 @@ function longPhrase(newArray) {
 		longWord = newArray[i];
 		}
 	}
-	console.log(longWord)
+	console.log("The longest word is " + longWord);
 }
-
-longPhrase(fruits);
-longPhrase(veggie);
-longPhrase(carBrand);
 
 // Release 1
 
@@ -29,8 +25,10 @@ longPhrase(carBrand);
 // Take two key-value pair objects as arguments
 // Create a conditional statement that returns true if at least one key-value pair match
 // If no key-value pair match, returns false
+
+
 function matchMaker(objOne, objTwo) {
-	outcome = '';
+	outcome= '';
 	if (objOne.name == objTwo.name || objOne.age == objTwo.age) {
 	outcome = true;
 	}else{
@@ -39,6 +37,36 @@ function matchMaker(objOne, objTwo) {
 	console.log(outcome);
 }
 
+// Release 2
+
+// Create function that takes an integer for length, and build and returns an array of strings of the given length
+// Take an integer as an argument to create the number of words to an array list
+// Declare a variable as letters of alphabet
+// Generate a random word with random index of alphabet list with a randomly created word length
+
+function randomWord(num){
+	for(var a = num; a > 0; --a){
+	function randomString(length) {
+	    var mask = 'abcdefghijklmnopqrstuvwxyz';
+	    var result = '';
+	    for (var i = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)];
+	    list.push(result);
+	    return result;
+	}
+	var wordLength = Math.floor((Math.random() * 10) + 1);
+	console.log(randomString(wordLength));
+	}
+}
+
+// Driver code
+var list = [];
+longPhrase(fruits);
+longPhrase(veggie);
+longPhrase(carBrand);
+console.log('----');
 matchMaker({name: "Robert", age: 24}, {name: "Jayce", age :24});
 matchMaker({name: "Eghert", age: 32}, {name: "Eghert", age :5});
 matchMaker({name: "Maggie", age: 18}, {name: "Ryder", age: 78});
+console.log('----');
+randomWord(3);
+longPhrase(list);
