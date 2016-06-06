32db311548fa7f5738279631cf40dffe703eb2ce
Daniel Yoon <sangchu3102@gmail.com>
2016-06-06 12:45:48 -0400

    Display correct calculations and message

--- databases/wk8chl/expiration_alert.rb
+++ databases/wk8chl/expiration_alert.rb
@@ -1,13 +1,15 @@
-# Expiration alert
+# EXPIRATION ALERT
 # This program is designed to give alerts when a purchased product nears its expiration date
+
 # Require gems
 require 'sqlite3'
+require 'time'
 
 # Create SQLite3 database
 db = SQLite3::Database.new("alert.db")
 db.results_as_hash = true
 
-# Use string delimiter to create products table
+# Use string delimiter that creates products table
 create_products_table = <<-SQL
 	CREATE TABLE IF NOT EXISTS products (
 		id INTEGER PRIMARY KEY,
@@ -20,25 +22,13 @@ create_products_table = <<-SQL
 		)
 SQL
 
-# create_types_table = <<-SQL
-# 	CREATE TABLE types (
-# 		id INTEGER PRIMARY KEY,
-# 		name VARCHAR(255)
-# 		)
-# SQL
-
 # Create products table
 db.execute(create_products_table)
 
-# Create types table
-# db.execute(create_types_table)
-
-# Add available types of products
-# db.execute("INSERT INTO types (name) VALUES ('vegetable'), ('fruit'), ('meat'), ('dairy'), ('medicine'), ('other')")
-
-# product = db.execute("SELECT * FROM products")
-# puts product.class
-# p product
+# Define method to insert user input into the table
+def insert_into_table(db, product, purch_date, exp_date, days_left, prod_type)
+	db.execute("INSERT INTO products (name, purchase_date, expiration_date, days_left, type_id) VALUES (?, ?, ?, ?, ?)",[product, purch_date, exp_date, days_left, prod_type])
+end
 
 # Create user input 
 loop do
@@ -47,55 +37,58 @@ loop do
 	if answer == "no"
 		break
 	elsif answer == "clear"
-		db.execute("DROP TABLE products")
+		db.execute("DELETE FROM products")
 		break
 	end
 
-	puts "Which product would you like to add?"
-	product = gets.chomp
+	valid = false
+	until valid
+		puts "Which product would you like to add?"
+		product = gets.chomp
+		if product == ""
+			valid = false
+			puts "Please make a valid input."
+		else valid = true
+		end
+	end
 
-	puts "What is the purchase date? Use YYYY,MM,DD format e.g. 2015, 11, 7"
-	purch_date = gets.chomp
+	valid = false
+	until valid
+		puts "What is the purchase date? Use YYYY-MM-DD format e.g. 2015-11-7"
+		purch_date = gets.chomp
+		if purch_date == ""
+			valid = false
+			puts "Please make a valid input."
+		else valid = true
+		end
+	end
 
-	puts "What is the expiration date? Use YYYY,MM,DD format e.g. 2016, 5, 29"
-	exp_date = gets.chomp
-	days_left = ((Time.new(exp_date) - Time.now).to_i) / 86400
+	valid = false
+	until valid
+		puts "What is the expiration date? Use YYYY-MM-DD format e.g. 2016-5-29"
+		exp_date = gets.chomp
+		if exp_date == ""
+			valid = false
+			puts "Please make a valid input."
+		else valid = true
+		end
+		days_left = ((Time.parse(exp_date) - Time.now).to_i) / 86400
+	end
 		
 	puts "What type of product is it? (1 for vegetable, 2 for fruit, 3 for meat, 4 for dairy, 5 medicine, 6 for other)"
 	prod_type = gets.chomp.to_i
 
-	db.execute("INSERT INTO products (name, purchase_date, expiration_date, days_left, type_id) VALUES (?, ?, ?, ?, ?)",[product, purch_date, exp_date, days_left, prod_type])
+	insert_into_table(db, product, purch_date, exp_date, days_left, prod_type)
 end
 
-# Calculate the days left before expiration and print an alert message
-
+# Display alert message
 products = db.execute("SELECT * FROM products")
 products.each do |product|
 	if product['days_left'] > 5
-		puts "The #{product['name']} that was purchased on #{product['purchase_date']} have #{product['days_left']} days before expiration."
-	elsif product['days_left'] > 0
-		puts "WARNING: The #{product['name']} that was purchased on #{product['purchase_date']} have #{product['days_left']} days before expiration."
-	else product['days_left'] >= -1
-		puts "PRODUCT EXPIRED, do not use the product please dispose of it."
+		puts "The #{product['name']} purchased on #{product['purchase_date']} have #{product['days_left']} days before expiration."
+	elsif product['days_left'] >= 0
+		puts "WARNING: The #{product['name']} purchased on #{product['purchase_date']} only have #{product['days_left']} days before expiration."
+	elsif
+		puts "PRODUCT EXPIRED, do not use #{product['name']} purchased on #{product['purchase_date']} and please dispose of it."
 	end
 end
-
-
-# Explore ORM by retrieving data
-# products = db.execute("SELECT * FROM products")
-# products.each do |product|
-# 	puts "#{product['name'].capitalize} was purchased on #{product['purchase_date']} and expires on #{product['expiration_date']}"
-# end
-
-
-
-
-
-
-
-
-
-
-
-
-
